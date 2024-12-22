f39a6b45f1bc2c0f422d94c2c035f882fe937bde
AlexMorrow239 <alex.morrow239@gmail.com>
2024-12-21 23:31:46 -0500

    can now add other forms of @miami.edu addresses.

--- src/modules/professors/pipes/email-domain.pipe.ts
+++ src/modules/professors/pipes/email-domain.pipe.ts
@@ -1,11 +1,18 @@
-import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
-import { CreateProfessorDto } from '../dto/create-professor.dto';
+import { Injectable, PipeTransform, BadRequestException } from "@nestjs/common";
+import { CreateProfessorDto } from "../dto/create-professor.dto";
 
 @Injectable()
 export class EmailDomainPipe implements PipeTransform {
   transform(value: CreateProfessorDto) {
-    if (!value.email.endsWith('@miami.edu')) {
-      throw new BadRequestException('Email must be a valid miami.edu address');
+    const validDomains = [
+      '@miami.edu',
+      '@med.miami.edu',
+      '@cd.miami.edu'
+      // Add other valid Miami domains as needed
+    ];
+    
+    if (!validDomains.some(domain => value.email.endsWith(domain))) {
+      throw new BadRequestException('Email must be a valid University of Miami address');
     }
     return value;
   }
