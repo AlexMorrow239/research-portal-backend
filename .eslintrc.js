3d3961d8d3638b94fe69079fe88b4c874bebf1ea
AlexMorrow239 <alex.morrow239@gmail.com>
2024-12-26 19:22:53 -0500

    updated formatting files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,62 @@
-export const parser = '@typescript-eslint/parser';
-export const parserOptions = {
-  project: './tsconfig.json',
-  sourceType: 'module',
-};
-export const plugins = ['@typescript-eslint', 'prettier'];
-export const extendConfigs = [
-  'plugin:@typescript-eslint/recommended',
-  'plugin:prettier/recommended',
-];
-export const root = true;
-export const env = {
-  node: true,
-  jest: true,
-};
-export const rules = {
-  '@typescript-eslint/interface-name-prefix': 'off',
-  '@typescript-eslint/explicit-function-return-type': 'off',
-  '@typescript-eslint/explicit-module-boundary-types': 'off',
-  '@typescript-eslint/no-explicit-any': 'off',
-};
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'import',
+    'prettier'
+  ],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules', 'coverage'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { 
+      'argsIgnorePattern': '^_',
+      'varsIgnorePattern': '^_',
+    }],
+    'import/order': ['error', {
+      'groups': [
+        ['builtin', 'external'],
+        'internal',
+        ['parent', 'sibling', 'index']
+      ],
+      'newlines-between': 'always',
+      'alphabetize': {
+        'order': 'asc',
+        'caseInsensitive': true
+      }
+    }],
+    'import/no-unresolved': 'error',
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'eqeqeq': ['error', 'always'],
+    'no-return-await': 'off',
+    'require-await': 'error'
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: 'tsconfig.json',
+      },
+    },
+  },
+}
