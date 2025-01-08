f97013bf7cd8a7fb66ee6e6f89a543fa0a90d06f
AlexMorrow239 <alex.morrow239@gmail.com>
2025-01-08 16:15:05 -0500

    added all umiami colleges

--- src/common/enums/application.enums.ts
+++ src/common/enums/application.enums.ts
@@ -15,6 +15,17 @@ export enum Citizenship {
 
 export enum College {
   ARTS_AND_SCIENCES = 'College of Arts and Sciences',
+  ARCHITECTURE = 'School of Architecture',
+  BUSINESS = 'Miami Herbert Business School',
+  COMMUNICATION = 'School of Communication',
+  EDUCATION = 'School of Education & Human Development',
+  ENGINEERING = 'College of Engineering',
+  LAW = 'School of Law',
+  MARINE_SCIENCE = 'Rosenstiel School of Marine, Atmospheric, and Earth Science',
+  MEDICINE = 'Miller School of Medicine',
+  MUSIC = 'Frost School of Music',
+  NURSING = 'School of Nursing and Health Studies',
+  GRAD = 'The Graduate School',
 }
 
 export enum WeeklyAvailability {
